31d8be1114164a475456dce9d391b53ab915f694
MyMiDiII <mymidi.student@yandex.ru>
2022-05-08 22:28:04 +0300

    ADD: project base

---
+++ src/BoardGames/BoardGames/Data/BoardGame.cs
@@ -0,0 +1,10 @@
+﻿// public ???
+namespace BoardGames.Data
+{
+    public class BoardGame
+    {
+        public string Title { get; set; }
+        public string Producer { get; set; }
+        public uint Year { get; set; }
+    }
+}
